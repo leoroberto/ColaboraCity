83f0a61b3e518398a17c8f2089b9e6d0fdb650a2
Leonardo Farias <leonardo.r.farias@avanade.com>
2018-08-02 23:35:07 -0300

    Criação da toolbar e criação do alert para inclusão dos dados

--- app/src/main/java/br/com/lfsolucoes/colaboracity/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/lfsolucoes/colaboracity/ui/activity/MainActivity.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.widget.Button
 import br.com.lfsolucoes.colaboracity.R
 import br.com.lfsolucoes.colaboracity.model.Problem
 import br.com.lfsolucoes.colaboracity.ui.adapter.ProblemListAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v4.view.MenuItemCompat
+import android.support.v7.widget.SearchView
+import android.support.v7.widget.ShareActionProvider
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.*
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +35,73 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = ProblemListAdapter(problems(), this)
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
+
+        // Aqui é a mágica (A Toolbar será a action bar).
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Agora podemos continuar usando a action bar normalmente
+        supportActionBar?.title = "ColaboraCity"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Infla o menu com os botões da action bar
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        // SearchView
+        val item = menu.findItem(R.id.action_search)
+        val searchView = item.actionView as SearchView
+        searchView.setOnQueryTextListener(onSearch())
+
+        // ShareActionProvider
+        val shareItem = menu.findItem(R.id.action_share)
+        val share = MenuItemCompat.getActionProvider(shareItem) as ShareActionProvider
+        share.setShareIntent(defaultIntent)
+
+        return true
+    }
+
+    // Intent que define o conteúdo que será compartilhado
+    private val defaultIntent: Intent
+        get() {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/*"
+            intent.putExtra(Intent.EXTRA_TEXT, "Texto para compartilhar")
+            return intent
+        }
+
+    private fun onSearch(): SearchView.OnQueryTextListener {
+        return object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                toast("Buscar o texto: " + query)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_search) {
+            toast("Clicou no Search!")
+            return true
+        } else if (id == R.id.action_refresh) {
+            toast("Clicou no Refresh!")
+            return true
+        } else if (id == R.id.action_settings) {
+            toast("Clicou no Settings!")
+            return true
+        } else if(id == R.id.action_add){
+            criarJanelaDeCadastro()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun toast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 
     private fun problems(): List<Problem> {
@@ -49,8 +123,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun criarJanelaDeCadastro() {
+        lateinit var tNone: EditText
+        lateinit var tDescricao: EditText
+        lateinit var tTipo: Spinner
+
+        val problemsArray = arrayOf("Buraco", "Sinal Quebrado")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, problemsArray)
+
+
+
+        alert {
+            title = "Cadastrar Problema"
+            isCancelable = false
+            customView {
+                verticalLayout {
+                    tNone = editText {
+                        hint = "Digite o nome"
+                    }
+                    tDescricao = editText {
+                        hint = "Digite a descrição"
+                    }
+
+                    tTipo = spinner {adapter = arrayAdapter}
+                }
+            }
+            okButton {
+                //Ação de salvar no firebase
+                toast("Salvou!")
+            }
+            cancelButton {  }
+        }.show()
+    }
+
     private fun logoutUser(){
         FirebaseAuth.getInstance().signOut()
         finish()
     }
+
+
 }
